7e898c6f79d2aa401fd0ca6a769e10ec7123685c
Vladimir Eliseev <ev1ad@yandex.ru>
2014-02-16 17:44:13 +0400

    + picture and floaing point validator

--- noc_labs/test_gui_ssm.tcl
+++ noc_labs/test_gui_ssm.tcl
@@ -24,7 +24,9 @@ proc SSModelDisplayData {f varN varM varK x0 A B C D} {
 	    } elseif {![info exist $f.A_${j}_$i]} {
 		set $f.A_${j}_$i [lindex $A $j $i]
 	    }
-	    entry $f.eA_${j}_$i -width 10 -textvariable $f.A_${j}_$i
+	    entry $f.eA_${j}_$i -width 10 \
+		-validate key -vcmd {string is double %P} \
+		-textvariable $f.A_${j}_$i
 	    grid $f.eA_${j}_$i -row [expr $r + $j] -column [expr $c + $i]
 	}
     }
@@ -41,7 +43,9 @@ proc SSModelDisplayData {f varN varM varK x0 A B C D} {
 	    } elseif {![info exist $f.B_${j}_$i]} {
 		set $f.B_${j}_$i [lindex $B $j $i]
 	    }
-	    entry $f.eB_${j}_$i -width 10 -textvariable $f.B_${j}_$i
+	    entry $f.eB_${j}_$i -width 10 \
+		-validate key -vcmd {string is double %P} \
+		-textvariable $f.B_${j}_$i
 	    grid $f.eB_${j}_$i -row [expr $r + $j] -column [expr $c + $i]
 	}
     }
@@ -60,7 +64,9 @@ proc SSModelDisplayData {f varN varM varK x0 A B C D} {
 	    } elseif {![info exist $f.C_${j}_$i]} {
 		set $f.C_${j}_$i [lindex $C $j $i]
 	    }
-	    entry $f.eC_${j}_$i -width 10 -textvariable $f.C_${j}_$i
+	    entry $f.eC_${j}_$i -width 10 \
+		-validate key -vcmd {string is double %P} \
+		-textvariable $f.C_${j}_$i
 	    grid $f.eC_${j}_$i -row [expr $r + $j] -column [expr $c + $i]
 	}
     }
@@ -77,7 +83,9 @@ proc SSModelDisplayData {f varN varM varK x0 A B C D} {
 	    } elseif {![info exist $f.D_${j}_$i]} {
 		set $f.D_${j}_$i [lindex $D $j $i]
 	    }
-	    entry $f.eD_${j}_$i -width 10 -textvariable $f.D_${j}_$i
+	    entry $f.eD_${j}_$i -width 10 \
+		-validate key -vcmd {string is double %P} \
+		-textvariable $f.D_${j}_$i
 	    grid $f.eD_${j}_$i -row [expr $r + $j] -column [expr $c + $i]
 	}
     }
@@ -94,7 +102,9 @@ proc SSModelDisplayData {f varN varM varK x0 A B C D} {
 	} elseif {![info exist $f.x0_$i]} {
 	    set $f.x0_$i [lindex $x0 $i]
 	}
-	entry $f.ex0_$i -width 10 -textvariable $f.x0_$i
+	entry $f.ex0_$i -width 10 \
+	    -validate key -vcmd {string is double %P} \
+	    -textvariable $f.x0_$i
 	grid $f.ex0_$i -row [expr $r] -column [expr $c + $i]
     }
     incr r
@@ -132,8 +142,10 @@ proc SSModelEditor {p thisvar} {
     set $w.outputs $m
     set $w.inputs $k
 
-    frame $w.dimensions
-    set f $w.dimensions
+    frame $w.info
+
+    frame $w.info.dimensions
+    set f $w.info.dimensions
     label $f.lstates -text "States:"
     label $f.linputs -text "Inputs:"
     label $f.loutputs -text "Outputs:"
@@ -145,7 +157,18 @@ proc SSModelEditor {p thisvar} {
 		    {$this(x0)} {$this(a)} {$this(b)} {$this(c)} {$this(d)}"
 	grid $f.l$x $f.e$x
     }
-    pack $f -anchor nw
+
+    # Let's find image to illustrate the function
+    #set imgfile [file join [TemplateDir] "ssmodel.gif"]
+    set imgfile templates/ssmodel.gif
+    #puts "check for $imgfile"
+    if {[file exists $imgfile]} {
+	set img [image create photo -file $imgfile]
+	label $w.info.picture -image $img
+    }
+    grid $w.info.dimensions $w.info.picture -padx 5
+
+    pack $w.info -anchor nw
 
     SSModelDisplayData $w.data $w.states $w.outputs $w.inputs \
 	$this(x0) $this(a) $this(b) $this(c) $this(d)
