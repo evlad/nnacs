9ed03704d828b6a8177ff794c608d004741b0b6b
Vladimir Eliseev <YeliseevVL@mpei.ru>
2015-10-29 01:21:02 +0300

    New version 1.7c

--- cmake_install.sh
+++ cmake_install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/nnacs-1.7b . && make all install
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/nnacs-1.7c . && make all install
 cp noc_labs/nnacs.desktop ~/.local/share/applications/nnacs.desktop
