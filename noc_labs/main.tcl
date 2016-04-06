1e6aa557c57e056f38054134ac0596ce3efd619b
Vladimir Eliseev <YeliseevVL@mpei.ru>
2016-04-07 01:24:00 +0300

    Version 1.8.1

--- noc_labs/main.tcl
+++ noc_labs/main.tcl
@@ -55,8 +55,8 @@ NNACS - Neural network applications for control systems
 } center
     global tcl_platform
     set platform [string totitle $tcl_platform(platform)]
-    $w.text insert end "Версия: 1.8.0 для $platform\n" center
-    $w.text insert end "Дата: 15 марта 2016 года\n" center
+    $w.text insert end "Версия: 1.8.1 для $platform\n" center
+    $w.text insert end "Дата: 6 апреля 2016 года\n" center
     $w.text insert end {
 Национальный исследовательский университет "МЭИ"
 Институт Автоматики и вычислительной техники
