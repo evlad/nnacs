2919d664ff134c60ff842746f50e64e04a5d48c2
Vladimir Eliseev <YeliseevVL@mpei.ru>
2016-04-17 15:57:46 +0300

    + test for equality of TF and SSM

--- test_mimo/test_mimo.sh
+++ test_mimo/test_mimo.sh
@@ -1,4 +1,9 @@
+echo "Test #1: etalon calculations"
 ../dtools/dtf test.ssm u.dat y_.dat
 diff -u y.dat y_.dat
 ../dtools/dtf test_2x2.ssm u2.dat y2_.dat
 diff -u y2.dat y2_.dat
+echo "Test #2: TF and SSM equality"
+../dtools/dtf plant.tf u_step.dat y_tf.dat
+../dtools/dtf plant.ssm u_step.dat y_ssm.dat
+diff -u y_tf.dat y_ssm.dat
