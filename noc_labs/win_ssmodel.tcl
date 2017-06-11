cdb8f3984bdc7bafca7ddfa3b2df5b6439c5bd0e
Vladimir Eliseev <ev1ad@yandex.ru>
2017-06-12 00:21:37 +0300

    + special window editor for SSM

--- noc_labs/win_ssmodel.tcl
+++ noc_labs/win_ssmodel.tcl
@@ -4,11 +4,15 @@ package provide win_ssmodel 1.0
 package require Tk
 package require files_loc
 package require universal
+package require ssm_file
+package require balloon
+
 
 # Format of internal SSM representation:
 # { name value ... }
 # where name-value pairs are in arbitrary order (adopted to be array).
 # Special name="name" is a name of StateSpaceModel (optional).
+# Special name="version" is a version of StateSpaceModel file, "1.0" for example (optional).
 # Value may be list.
 
 # Table of obliratory parameters their meaning:
@@ -26,226 +30,346 @@ package require universal
 # --------+------------------------------------------------
 
 # For example:
-# { name "ssm1" states 2 inputs 1 outputs 2
+# { states 2 inputs 1 outputs 2
 #   a {{1 0.5} {0 2}} b {0 1} c {{1 0} {0 1}} d {0 0} x0 {0 0} }
 
-# Load object from given text taken from file.
-# - this - object internal data (array with keys) - result;
-# - ftext - file contents to parse (.ssm format).
-proc SSModelLoadConfig {thisvar ftext} {
-    upvar $thisvar this
 
-    foreach line $ftext {
-	# Let's exclude empty items to get fields
-	set fields {}
-	foreach f [split $line] {
-	    if {$f != {}} {
-		lappend fields $f
+# Display frame $f with grid of editable cells
+# - varN - name of variable with number of states
+# - varM - name of variable with number of outputs
+# - varK - name of variable with number of inputs
+# - x0, A, B, C, D - vector and matrices of initial values for the grid
+# Result in global variables:
+#  $f.A_(0..$N-1)_(0..$N-1)
+#  $f.B_(0..$N-1)_(0..$K-1)
+#  $f.C_(0..$M-1)_(0..$N-1)
+#  $f.D_(0..$M-1)_(0..$K-1)
+#  $f.x0_(0..$N-1)
+proc SSModelDisplayData {f varN varM varK x0 A B C D} {
+    upvar #0 $varN n
+    upvar #0 $varM m
+    upvar #0 $varK k
+
+    # set to 1 to display index in every cell
+    set debugCells 0
+
+    frame $f
+
+    # resource values
+    set cellwidth 8
+    set defvalue 0
+    array set bgc {A pink B LightCyan C Lavender D PaleGreen x0 Bisque}
+
+    set r 0
+    set c 0
+
+    label $f.lA -text "A:" -background $bgc(A)
+    grid $f.lA -row $r -column $c
+    incr c
+    for {set j 0} {$j < $n} {incr j} {
+	for {set i 0} {$i < $n} {incr i} {
+	    global $f.A_${j}_$i
+	    if $debugCells {
+		set $f.A_${j}_$i "A\[$j,$i\]"
+	    } else {
+		set cellval [lindex $A $j $i]
+		if {{} == $cellval} {
+		    set $f.A_${j}_$i $defvalue
+		} else {
+		    set $f.A_${j}_$i $cellval
+		}
 	    }
-	}
-	# Let's try to find key in the line
-	#puts "key_pos: $key_pos"
-	foreach {key pos} $key_pos {
-	    # Key presents somewhere after ;
-	    set cindex [lsearch -exact $fields ";"]
-	    if {$cindex > 0} {
-		# There is a comment, let's find all keywords
-		if {[lsearch -exact [lrange $fields $cindex end] $key] > 0} {
-		    set this($key) [lindex $fields $pos]
-		    #puts "[lindex $fields $pos] ==> $key=$this($key)"
-		} 
-	    }
+	    entry $f.eA_${j}_$i -width $cellwidth \
+		-validate key -vcmd {string is double %P} \
+		-textvariable $f.A_${j}_$i \
+		-background $bgc(A)
+	    balloon $f.eA_${j}_$i -text "A([expr 1+$j],[expr 1+$i])"
+	    grid $f.eA_${j}_$i -row [expr $r + $j] -column [expr $c + $i]
 	}
     }
+
+    incr c $n
+    label $f.lB -text "B:" -background $bgc(B)
+    grid $f.lB -row $r -column [expr $c]
+    incr c
+    for {set j 0} {$j < $n} {incr j} {
+	for {set i 0} {$i < $k} {incr i} {
+	    global $f.B_${j}_$i
+	    if $debugCells {
+		set $f.B_${j}_$i "B\[$j,$i\]"
+	    } else {
+		set cellval [lindex $B $j $i]
+		if {{} == $cellval} {
+		    set $f.B_${j}_$i $defvalue
+		} else {
+		    set $f.B_${j}_$i $cellval
+		}
+	    }
+	    entry $f.eB_${j}_$i -width $cellwidth \
+		-validate key -vcmd {string is double %P} \
+		-textvariable $f.B_${j}_$i \
+		-background $bgc(B)
+	    balloon $f.eB_${j}_$i -text "B([expr 1+$j],[expr 1+$i])"
+	    grid $f.eB_${j}_$i -row [expr $r + $j] -column [expr $c + $i]
+	}
+    }
+    incr r $n
+
+    incr r
+    set c 0
+    label $f.lC -text "C:" -background $bgc(C)
+    grid $f.lC -row $r -column $c
+    incr c
+    for {set j 0} {$j < $m} {incr j} {
+	for {set i 0} {$i < $n} {incr i} {
+	    global $f.C_${j}_$i
+	    if $debugCells {
+		set $f.C_${j}_$i "C\[$j,$i\]"
+	    } else {
+		set cellval [lindex $C $j $i]
+		if {{} == $cellval} {
+		    set $f.C_${j}_$i $defvalue
+		} else {
+		    set $f.C_${j}_$i $cellval
+		}
+	    }
+	    entry $f.eC_${j}_$i -width $cellwidth \
+		-validate key -vcmd {string is double %P} \
+		-textvariable $f.C_${j}_$i \
+		-background $bgc(C)
+	    balloon $f.eC_${j}_$i -text "C([expr 1+$j],[expr 1+$i])"
+	    grid $f.eC_${j}_$i -row [expr $r + $j] -column [expr $c + $i]
+	}
+    }
+
+    incr c $n
+    label $f.lD -text "D:" -background $bgc(D)
+    grid $f.lD -row $r -column [expr $c]
+    incr c
+    for {set j 0} {$j < $m} {incr j} {
+	for {set i 0} {$i < $k} {incr i} {
+	    global $f.D_${j}_$i
+	    if $debugCells {
+		set $f.D_${j}_$i "D\[$j,$i\]"
+	    } else  {
+		set cellval [lindex $D $j $i]
+		if {{} == $cellval} {
+		    set $f.D_${j}_$i $defvalue
+		} else {
+		    set $f.D_${j}_$i $cellval
+		}
+	    }
+	    entry $f.eD_${j}_$i -width $cellwidth \
+		-validate key -vcmd {string is double %P} \
+		-textvariable $f.D_${j}_$i \
+		-background $bgc(D)
+	    balloon $f.eD_${j}_$i -text "D([expr 1+$j],[expr 1+$i])"
+	    grid $f.eD_${j}_$i -row [expr $r + $j] -column [expr $c + $i]
+	}
+    }
+    incr r $m
+
+    set c 0
+    label $f.lx0 -text "x0:" -background $bgc(x0)
+    grid $f.lx0 -row $r -column $c
+    incr c
+    for {set i 0} {$i < $n} {incr i} {
+	global $f.x0_$i
+	if $debugCells {
+	    set $f.x0_$i "x0\[$i\]"
+	} else {
+	    set cellval [lindex $x0 $i]
+	    if {{} == $cellval} {
+		set $f.x0_$i $defvalue
+	    } else {
+		set $f.x0_$i $cellval
+	    }
+	}
+	entry $f.ex0_$i -width $cellwidth \
+	    -validate key -vcmd {string is double %P} \
+	    -textvariable $f.x0_$i \
+	    -background $bgc(x0)
+	balloon $f.ex0_$i -text "x0([expr 1+$i])"
+	grid $f.ex0_$i -row [expr $r] -column [expr $c + $i]
+    }
+    incr r
+
+    pack $f
 }
 
-
-# Display four matrices of the object
+# Display parameters of the State-Space object
 # - p - parent widget
 # - thisvar - name of the array in calling context to list name=value pairs
-# - descr - description of the function: name, type, label, parameters
-# Returns: 1 - if there were changes; 0 - there were not changes in
-# parameters
+# Returns:
+#  1 - if there were changes;
+#  0 - there were no changes in any parameters
 proc SSModelEditor {p thisvar} {
-    set w $p.ssmeditor
+    set w $p.ssmodel
     catch {destroy $w}
     toplevel $w
 
     upvar $thisvar this
+    set n $this(states)
+    set m $this(outputs)
+    set k $this(inputs)
 
-    wm title $w "State Space Model"
+    # +---+--+--+----+--+--+
+    # |A: |  |  | B: |  |  |
+    # +---+--+--+----+--+--+
+    # |   |  |  |    |  |  |
+    # +---+--+--+----+--+--+
+    # |C: |  |  | D: |  |  |
+    # +---+--+--+----+--+--+
+    # |x0:|  |  |
+    # +---+--+--+
 
-    frame $w.common
-    label $w.common.main_label -text $label -anchor w
-    pack $w.common.main_label
+    global $w.states $w.inputs $w.outputs
+    set $w.states $n
+    set $w.outputs $m
+    set $w.inputs $k
 
-    frame $w.dimensions
-    
+    frame $w.info
+
+    frame $w.info.dimensions
+    set f $w.info.dimensions
+    label $f.lstates -text "States:"
+    label $f.linputs -text "Inputs:"
+    label $f.loutputs -text "Outputs:"
+
+    foreach x {states inputs outputs} {
+	spinbox $f.e$x -width 3 -from 1 -to 10 -textvariable $w.$x \
+	    -command "destroy $w.data ; \
+		SSModelDisplayData $w.data $w.states $w.outputs $w.inputs \
+		    {$this(x0)} {$this(a)} {$this(b)} {$this(c)} {$this(d)}"
+	grid $f.l$x $f.e$x
+    }
 
     # Let's find image to illustrate the function
-    foreach ext {gif ppm pgm} {
-	set imgfile [file join [TemplateDir] "$idname.$ext"]
-	#puts "check for $imgfile"
-	if {[file exists $imgfile]} {
-	    set img [image create photo -file $imgfile]
-	    label $w.common.image -image $img
-	    pack $w.common.image -side top -padx 3 -pady 3
-	    break
-	}
+    set imgfile [file join [TemplateDir] "ssmodel.gif"]
+    #set imgfile templates/ssmodel.gif
+    if {[file exists $imgfile]} {
+	set img [image create photo -file $imgfile]
+	label $w.info.picture -image $img
     }
-    pack $w.common -side top
+    grid $w.info.dimensions $w.info.picture -padx 5
 
-    frame $w.parameters
-    # This variable consist of all operations to store state of dialog
-    set save_all_vars $w.save_all_vars
-    global $save_all_vars
-    set $save_all_vars ""
-    foreach {key pos} $key_pos {
-	label $w.parameters.label_$key -text $key
-	set e $w.parameters.value_$key
-	# This variable is global
-	set evar $w.var_$key
-	global $evar
-	if {[info exists this($key)]} {
-	    set $evar $this($key)
-	} else {
-	    set $evar ""
-	}
-	entry $e -width 12 -justify right -validate focus \
-	    -vcmd {string is double %P}
-	$e insert 0 [set $evar]
-	set $save_all_vars "set $evar \[$e get\] ; [set $save_all_vars]"
-	$e configure -invalidcommand "focusAndFlash %W [$e cget -fg] [$e cget -bg]"
-	grid $w.parameters.label_$key $e
-    }
-    pack $w.parameters -side top
+    pack $w.info -anchor nw
+
+    SSModelDisplayData $w.data $w.states $w.outputs $w.inputs \
+	$this(x0) $this(a) $this(b) $this(c) $this(d)
+
 
     frame $w.buttons
     pack $w.buttons -side bottom -fill x -pady 2m
-    button $w.buttons.ok -text "OK" -command "[set $save_all_vars] destroy $w"
-    button $w.buttons.cancel -text "Отмена" -command "destroy $w"
 
-    set m $w.buttons.probe.m
-    menubutton $w.buttons.probe -text "Отклик" \
-	-direction below -menu $m -relief raised
-    menu $m -tearoff 0
-    foreach probesignal {pulse step sin_4 sin_10 sin_20} {
-	$m add command -label $probesignal \
-	    -command "TrFuncProbeTemporal $w [list $descr] $probesignal"
-    }
-    #grid $w.buttons.probe -row 1 -column 0 -sticky n
+    global $w.ok_pressed
+    set $w.ok_pressed 0
 
-    pack $w.buttons.ok $w.buttons.probe $w.buttons.cancel -side left -expand 1
+    button $w.buttons.ok -text "OK" \
+	-command "global $w.ok_pressed; set $w.ok_pressed 1; destroy $w"
+    button $w.buttons.cancel -text "Cancel" -command "destroy $w"
+    pack $w.buttons.ok $w.buttons.cancel -side left -expand 1
 
     tkwait window $w
 
-    # Assign stored state
     set changed 0
-    foreach {key pos} $key_pos {
-	set evar $w.var_$key
-	global $evar
-	if {$this($key) != [set $evar]} {
-	    set this($key) [set $evar]
-	    #puts "$key=$this($key)"
+    if {[set $w.ok_pressed]} {
+
+	# Check for changed dimensions
+	foreach {key evar} "states $w.states outputs $w.outputs inputs $w.inputs" {
+	    if {$this($key) != [set $evar]} {
+		set this($key) [set $evar]
+		set changed 1
+	    }
+	}
+
+	if {$changed } {
+	    set n $this(states)
+	    set m $this(outputs)
+	    set k $this(inputs)
+	}
+
+	# Store all values from grid into $this
+	set f $w.data
+
+	set new_a {}
+	for {set j 0} {$j < $n} {incr j} {
+	    set new_row {}
+	    for {set i 0} {$i < $n} {incr i} {
+		global $f.A_${j}_$i
+		if {[info exist $f.A_${j}_$i]} {
+		    lappend new_row [set $f.A_${j}_$i]
+		}
+	    }
+	    lappend new_a $new_row
+	}
+	if {$this(a) != $new_a} {
+	    set this(a) $new_a
+	    set changed 1
+	}
+
+	set new_b {}
+	for {set j 0} {$j < $n} {incr j} {
+	    set new_row {}
+	    for {set i 0} {$i < $k} {incr i} {
+		global $f.B_${j}_$i
+		if {[info exist $f.B_${j}_$i]} {
+		    lappend new_row [set $f.B_${j}_$i]
+		}
+	    }
+	    lappend new_b $new_row
+	}
+	if {$this(b) != $new_b} {
+	    set this(b) $new_b
+	    set changed 1
+	}
+
+	set new_c {}
+	for {set j 0} {$j < $m} {incr j} {
+	    set new_row {}
+	    for {set i 0} {$i < $n} {incr i} {
+		global $f.C_${j}_$i
+		if {[info exist $f.C_${j}_$i]} {
+		    lappend new_row [set $f.C_${j}_$i]
+		}
+	    }
+	    lappend new_c $new_row
+	}
+	if {$this(c) != $new_c} {
+	    set this(c) $new_c
+	    set changed 1
+	}
+
+	set new_d {}
+	for {set j 0} {$j < $m} {incr j} {
+	    set new_row {}
+	    for {set i 0} {$i < $k} {incr i} {
+		global $f.D_${j}_$i
+		if {[info exist $f.D_${j}_$i]} {
+		    lappend new_row [set $f.D_${j}_$i]
+		}
+	    }
+	    lappend new_d $new_row
+	}
+	if {$this(d) != $new_d} {
+	    set this(d) $new_d
+	    set changed 1
+	}
+
+	set new_x0 {}
+	for {set i 0} {$i < $n} {incr i} {
+	    global $f.x0_$i
+	    if {[info exist $f.x0_$i]} {
+		lappend new_x0 [set $f.x0_$i]
+	    }
+	}
+	if {$this(x0) != $new_x0} {
+	    set this(x0) $new_x0
 	    set changed 1
 	}
     }
+
     return $changed
-}
-
-
-# Call file state space model editor
-# - forceNew - boolean: true to create new file anyway
-# - asText - boolean: true to edit as text file
-proc SSModelEdit {p sessionDir title fileRelPath {forceNew false} {asText false}} {
-    puts "SSModelEdit: $sessionDir $fileRelPath"
-    set ftype [TrFuncNewType $p $sessionDir $fileRelPath $forceNew]
-    if {$forceNew && $ftype == {}} {
-	# User cancelled transfer function creation
-	return
-    }
-    set fileName [SessionAbsPath $sessionDir $fileRelPath]
-    # Now it's possible to edit the file
-
-    # Let's determine type of the file
-    switch -glob -- $fileName {
-	*.tf {
-	    set descr [TrFuncParseFile $fileName]
-	    if {[llength $descr] == 4 &&
-		[lindex $descr 0] != {} && [lindex $descr 1] != {} &&
-		[lindex $descr 2] != {} && [lindex $descr 3] != {}} {
-		# The whole definition is in the file
-		set ftype trfunc
-	    } elseif {[llength $descr] == 4 &&
-		      [lindex $descr 0] != {}} {
-		# Only idname was found: let's use template
-		set descr [TrFuncParseTemplate [lindex $descr 0]]
-		if {$descr != {}} {
-		    set ftype trfunc
-		} else {
-		    set ftype undefined
-		}
-	    } else {
-		set ftype undefined
-	    }
-	}
-	*.cof {
-	    set ftype cofunc
-	}
-	default {
-	    set ftype undefined
-	}
-    }
-    # Let's call edit method
-    if {$asText} {
-	set ftype undefined
-    }
-    switch -exact -- $ftype {
-	trfunc {
-	    # Load
-	    array set params {}
-	    set fd [open $fileName]
-	    set ftext [split [read $fd] \n]
-	    close $fd
-	    # Parse
-	    set idname [lindex $descr 0]
-	    set type [lindex $descr 1]
-	    set label [lindex $descr 2]
-	    set key_pos [lindex $descr 3]
-	    TrFuncLoadConfig params $descr $ftext
-	    # Edit
-	    if {[TrFuncEditor $p params $descr]} {
-		set headLineFields [split [lindex $ftext 0]]
-		set fd [open $fileName "w"]
-		if {[lindex $headLineFields 0] != ";NeuCon" &&
-		    [lindex $headLineFields 1] != "transfer" } {
-		    puts $fd ";NeuCon transfer 1.0"
-		    puts $fd "\[$type $idname\]"
-		}
-		# Save
-		TrFuncSaveConfig params $descr $fd $ftext
-		flush $fd
-		close $fd
-	    }
-	    # otherwise no changes took place
-	}
-	cofunc {
-	    # Load & parse
-	    array set cof {main {CombinedFunction {}}}
-	    catch {array set cof [CoFuncParseFile $fileName]}
-	    puts "LOAD: $fileName [array get cof]"
-	    # Edit
-	    if {[CoFuncEditor $p cof]} {
-		# Save
-		puts "SAVE: $fileName [array get cof]"
-		CoFuncComposeFile $fileName [array get cof]
-	    }
-	    # otherwise no changes took place
-	}
-	undefined {
-	    TextEditWindow $p "$title" $fileName
-	}
-    }
 }
 
 
